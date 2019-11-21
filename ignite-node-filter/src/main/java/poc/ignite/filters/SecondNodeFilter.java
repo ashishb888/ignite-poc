11545bc9b391ff13e80243d4296e8a3257847a6a
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-11-21 18:58:00 +0530

    ignite-node-filter: Initial commit

---
+++ ignite-node-filter/src/main/java/poc/ignite/filters/SecondNodeFilter.java
@@ -0,0 +1,21 @@
+package poc.ignite.filters;
+
+import org.apache.ignite.cluster.ClusterNode;
+import org.apache.ignite.lang.IgnitePredicate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SecondNodeFilter implements IgnitePredicate<ClusterNode> {
+
+	private static final long serialVersionUID = 1L;
+	public static String NAME = "SECOND";
+
+	@Override
+	public boolean apply(ClusterNode node) {
+		log.info("NAME: " + node.attributes().get("NAME"));
+		log.info("Node ID: " + node.id() + " toString : " + node.toString() + " Client: " + node.isClient());
+
+		return NAME.equals(node.attributes().get("NAME"));
+	}
+}
