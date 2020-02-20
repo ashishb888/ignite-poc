4eea1ba871d62cdce3993cba931e201c21ed9a9a
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-02-20 15:31:20 +0530

    ignite-transactions: Run 4

--- ignite-transactions/src/main/java/poc/ignite/service/TransactionService.java
+++ ignite-transactions/src/main/java/poc/ignite/service/TransactionService.java
@@ -9,7 +9,10 @@ import java.util.stream.IntStream;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteTransactions;
+import org.apache.ignite.Ignition;
 import org.apache.ignite.transactions.Transaction;
+import org.apache.ignite.transactions.TransactionConcurrency;
+import org.apache.ignite.transactions.TransactionIsolation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,8 +35,8 @@ public class TransactionService {
 	private AtomicBoolean baseTopology = new AtomicBoolean(false);
 	private String schema = "sts2";
 
-	private void start() {
-		log.debug("start service");
+	private void getAllAndPutAll() {
+		log.debug("getAllAndPutAll service");
 
 		int records = Integer.valueOf(ip.getOther().get("records"));
 		IgniteCache<Integer, Person> personCache = cr.personCache("person-cache2", schema, "Default_Region");
@@ -198,6 +201,7 @@ public class TransactionService {
 				log.debug("val: " + val);
 
 				val.setT2("t1");
+				val.setT2("t1");
 				personCache.put(key, val);
 
 				tx.commit();
@@ -239,11 +243,93 @@ public class TransactionService {
 		t3.start();
 	}
 
+	private void getAllAndPutAllWithTransaction() {
+		log.debug("getAllAndPutAllWithTransaction service");
+
+		int records = Integer.valueOf(ip.getOther().get("records"));
+		IgniteCache<Integer, Person> personCache = cr.personCache("person-cache2", schema, "Default_Region");
+
+		IntStream.iterate(0, i -> i + 1).limit(records).forEach(i -> {
+			personCache.put(i, new Person(i, "s1" + i, "s2" + i));
+		});
+
+		Set<Integer> keys = IntStream.iterate(0, i -> i + 1).limit(records).boxed().collect(Collectors.toSet());
+		// Set<Integer> keys = new HashSet<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8,
+		// 9));
+
+		Thread t1 = new Thread(() -> {
+			log.debug("t1 starts");
+
+			try (Transaction tx = Ignition.ignite().transactions().txStart()) {
+				Map<Integer, Person> personMp = personCache.getAll(keys);
+				log.debug("Original personMp: " + personMp);
+
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					log.error(e.getMessage(), e);
+				}
+
+				personMp.forEach((k, v) -> {
+					v.setT2("t1");
+				});
+
+				log.debug("Updated personMp: " + personMp);
+				personCache.putAll(personMp);
+
+				tx.commit();
+			}
+		}, "t1");
+
+		Thread t2 = new Thread(() -> {
+			log.debug("t2 starts");
+
+			try (Transaction tx = Ignition.ignite().transactions().txStart(TransactionConcurrency.PESSIMISTIC,
+					TransactionIsolation.READ_COMMITTED)) {
+				Map<Integer, Person> personMp = personCache.getAll(keys);
+				log.debug("Original personMp: " + personMp);
+
+//			try {
+//				Thread.sleep(5000);
+//			} catch (InterruptedException e) {
+//				log.error(e.getMessage(), e);
+//			}
+
+				personMp.forEach((k, v) -> {
+					v.setT1("t2");
+				});
+
+				log.debug("Updated personMp: " + personMp);
+				personCache.putAll(personMp);
+
+				tx.commit();
+			}
+		}, "t2");
+
+		Thread t3 = new Thread(() -> {
+			log.debug("t3 starts");
+
+			while (true) {
+				log.debug("personMp: " + personCache.getAll(keys));
+				try {
+					Thread.sleep(4000);
+				} catch (InterruptedException e) {
+					log.error(e.getMessage(), e);
+				}
+			}
+		}, "t3");
+
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+
 	public void main() {
 		log.debug("main service");
 
 		// start();
 		// getAndPut();
-		getAndPutWithTransaction();
+		// getAndPutWithTransaction();
+		getAllAndPutAllWithTransaction();
 	}
 }
